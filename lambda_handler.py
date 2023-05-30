f85d77cd3589dbaffbea7e23a542675b0ec67c43
Matt <mattwasilewski96@gmail.com>
2023-05-31 01:48:38 +0200

    added saving plates timestamp in s3

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,16 +2,18 @@ import boto3
 import json
 import logging
 import re
+from datetime import timedelta
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 class VideoDetect:
-    def __init__(self, role, bucket, video, rek):
+    def __init__(self, role, bucket, video, rek, results):
         self.roleArn = role
         self.bucket = bucket
         self.video = video
         self.rek = rek
+        self.results = results
 
     def StartTextDetection(self):
         response = self.rek.start_text_detection(
@@ -31,6 +33,14 @@ class VideoDetect:
         s3 = boto3.resource('s3')
         json_data = {'license_plates': detected_texts}
         s3.Object(self.bucket, 'output.json').put(Body=json.dumps(json_data))
+
+
+    def process_license_plate(self, license_plate, timestamp):
+        current_time = str(timedelta(milliseconds=timestamp))
+        if license_plate in self.results:
+            self.results[license_plate].append(current_time)
+        else:
+            self.results[license_plate] = [current_time]
     
 
     def GetTextDetectionResults(self):
@@ -47,14 +57,15 @@ class VideoDetect:
             if response['JobStatus'] == "IN_PROGRESS":
                 continue
             else:
-                detected_texts = []
                 for detection in response['TextDetections']:
                     detected_text = detection['TextDetection']['DetectedText']
+                    timestamp = detection['Timestamp']
                     if self.validate_license_plate(detected_text):
-                        detected_texts.append(detected_text)
+                        self.process_license_plate(detected_text, timestamp)
                         print(detected_text)
+                        print(timestamp)
 
-                self.save_data_s3(detected_texts)
+                self.save_data_s3(self.results)
                 finished = True
 
 
@@ -63,11 +74,12 @@ def lambda_handler(event, context):
     roleArn = event['role']
     bucket = event['bucket']
     video = event['file']
+    results = {}
 
     session = boto3.Session()
     rek = session.client('rekognition')
 
-    analyzer = VideoDetect(roleArn, bucket, video, rek)
+    analyzer = VideoDetect(roleArn, bucket, video, rek, results)
     analyzer.StartTextDetection()
     analyzer.GetTextDetectionResults()
 
