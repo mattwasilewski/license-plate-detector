8ae1b25032fb3d875c0a83c17ad8e73e875c0241
Matt <mattwasilewski96@gmail.com>
2023-06-01 18:55:15 +0200

    date removed from timestamp

--- s3_saver.py
+++ s3_saver.py
@@ -1,13 +1,8 @@
 import boto3
 import json
 import logging
-from datetime import datetime
 
 class S3Saver:
-    def default(self, o):
-        if isinstance(o, datetime):
-            return o.isoformat()
-
     def save_data_s3(self, bucket, results):
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
@@ -17,11 +12,10 @@ class S3Saver:
         for result in results:
             license_plate = result['license_plate']
             video_segments = result['video_segments']
-
             segment_list = []
             for segment in video_segments:
-                start_time = segment['start_time']
-                end_time = segment['end_time']
+                start_time = segment['start_time'].strftime('%H:%M:%S.%f')[:-3]
+                end_time = segment['end_time'].strftime('%H:%M:%S.%f')[:-3]
                 segment_data = {
                     'start_time': start_time,
                     'end_time': end_time
@@ -34,5 +28,5 @@ class S3Saver:
             }
             json_data.append(result_data)
 
-        s3.Object(bucket, 'output.json').put(Body=json.dumps(json_data, default=self.default))
+        s3.Object(bucket, 'output.json').put(Body=json.dumps(json_data))
         logging.info('Data saved in S3')
